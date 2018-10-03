3fd1a92a04076b24ef284416cd0affdb7eec8605
Hibri Marzook <hibri@hibri.net>
2018-10-03 16:40:28 +0100

    add all tool aliases

--- tool-aliases/aliases.zsh
+++ tool-aliases/aliases.zsh
@@ -1,1 +1,4 @@
 alias aws='docker run --rm -v ${HOME}/.aws:/root/.aws -v `pwd`:/project -it mesosphere/aws-cli:latest $*'
+alias az='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it microsoft/azure-cli:latest az $*'
+alias terraform='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hashicorp/terraform:light $*'
+alias inspec='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it chef/inspec:latest $*'
