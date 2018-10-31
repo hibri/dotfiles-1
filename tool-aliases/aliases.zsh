62fe5ae5a1184103f69e82bc40f6854a7ef24226
Hibri Marzook <hibri@hibri.net>
2018-10-31 14:29:10 +0000

    add packer

--- tool-aliases/aliases.zsh
+++ tool-aliases/aliases.zsh
@@ -1,5 +1,6 @@
 alias aws='docker run --rm -v ${HOME}/.aws:/root/.aws -v `pwd`:/project -it mesosphere/aws-cli:latest $*'
 alias az='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it microsoft/azure-cli:latest az $*'
-alias terraform='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hashicorp/terraform:light $*'
+alias tf='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hashicorp/terraform:light $*'
 alias inspec='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it chef/inspec:latest $*'
+alias packer='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hashicorp/packer:latest $*'
 alias ping='prettyping $*'
