a991c0f1fa8e93fbbba397952a9e246531540604
Hibri Marzook <hibri@hibri.net>
2018-12-10 17:16:38 +0000

    add terraform to path

--- tool-aliases/aliases.zsh
+++ tool-aliases/aliases.zsh
@@ -1,6 +1,13 @@
 alias aws='docker run --rm -v ${HOME}/.aws:/root/.aws -v `pwd`:/project -it mesosphere/aws-cli:latest $*'
 alias az='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it microsoft/azure-cli:latest az $*'
 alias tf='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hashicorp/terraform:light $*'
+alias terraform='tf'
 alias inspec='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it chef/inspec:latest $*'
 alias packer='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hashicorp/packer:latest $*'
 alias ping='prettyping $*'
+
+
+if (( $+commands[terraform] ))
+then
+  export PATH="$PATH:`terraform`"
+fi
