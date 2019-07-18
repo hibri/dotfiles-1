34dc71fd0a8d38b51cfe0fb64f69d68915350105
Hibri Marzook <hibri@hibri.net>
2019-07-18 15:01:43 +0100

    use my own docker images for terraform,

--- tool-aliases/aliases.zsh
+++ tool-aliases/aliases.zsh
@@ -1,6 +1,6 @@
 alias aws='docker run --rm -v ${HOME}/.aws:/root/.aws -v `pwd`:/project -it mesosphere/aws-cli:latest $*'
 alias az='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it microsoft/azure-cli:latest az $*'
-alias tf='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hashicorp/terraform:light $*'
+alias tf='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hibri/terraform $*'
 alias terraform='tf'
 alias inspec='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it chef/inspec:latest $*'
 alias packer='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hashicorp/packer:latest $*'
