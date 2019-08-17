7980d9432257631401a4d07db07027d6cbc840bd
Hibri Marzook <hibri@hibri.net>
2019-08-17 13:13:24 +0100

    remove docker and add az default sub

--- tool-aliases/aliases.zsh
+++ tool-aliases/aliases.zsh
@@ -1,10 +1,9 @@
 alias aws='docker run --rm -v ${HOME}/.aws:/root/.aws -v `pwd`:/project -it mesosphere/aws-cli:latest $*'
-alias az='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it microsoft/azure-cli:latest az $*'
-alias tf='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hibri/terraform $*'
-alias terraform='tf'
+alias tf='terraform'
 alias inspec='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it chef/inspec:latest $*'
 alias packer='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hashicorp/packer:latest $*'
 alias ping='prettyping $*'
+alias azdefault="az account list --query '[?isDefault]'"
 
 
 if (( $+commands[terraform] ))
